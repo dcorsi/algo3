c35007d68678caa765ce540f48fe6318f9d52b86
dcorsi <dcorsi@users.noreply.github.com>
2022-10-28 10:02:45 -0300

    Delete hellomundo directory

--- hellomundo/src/main/java/algo3/hellomundo/Modelo.java
+++
@@ -1,9 +0,0 @@
-package algo3.hellomundo;
-
-public class Modelo {
-
-	public String generarSaludo(String nombre) {
-		return "Hola, " + nombre + "!";
-	}
-}
-
--- hellomundo/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module algo3.hellomundo {
-    requires javafx.controls;
-	requires transitive javafx.graphics;
-	requires javafx.base;
-	exports algo3.hellomundo;
-}
