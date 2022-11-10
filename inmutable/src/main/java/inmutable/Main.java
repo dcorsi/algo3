e2a4e3849847269625561a0bb93efacd1ece9d90
dcorsi <dcorsi@users.noreply.github.com>
2022-11-10 18:30:22 -0300

    Update Main.java

--- inmutable/src/main/java/inmutable/Main.java
+++ inmutable/src/main/java/inmutable/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ArrayList<Integer> numeros = new ArrayList<>();
+		final ArrayList<Integer> numeros = new ArrayList<>();
 		numeros.add(1);
 		numeros.add(2);
 		numeros.add(3);
