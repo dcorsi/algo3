d39546973d1e999521de0e469c41b6879cf701ca
dcorsi <dcorsi@users.noreply.github.com>
2024-10-31 01:49:07 -0300

    Update core.clj

--- pokemones/src/pokemones/core.clj
+++ pokemones/src/pokemones/core.clj
@@ -1,7 +1,9 @@
 (ns pokemones.core
   (:require [clj-http.client :as client]))
 
+
 (declare codigo-paralelizable)
+
 
 (defn -main []
   (let [consulta (client/get "https://pokeapi.co/api/v2/pokemon-species?limit=100" {:as :json})
@@ -9,10 +11,12 @@
         resultados (:results cuerpo)
         direcciones (map :url resultados)
         pokemones (time (doall (codigo-paralelizable direcciones)))]
-    (do (run! println pokemones) (shutdown-agents))))
+    (do (run! #(println (str (first %) ": " (second %))) pokemones) (shutdown-agents))))
 
 (defn codigo-paralelizable [direcciones]
   (pmap #(-> %
-            (client/get ,,, {:as :json})
-            :body ,,,
-            (select-keys ,,, [:name :shape])) direcciones))
+             (client/get ,,, {:as :json})
+             :body ,,,
+             (select-keys ,,, [:name :shape])
+             (update :shape :name)
+             (vals)) direcciones))
