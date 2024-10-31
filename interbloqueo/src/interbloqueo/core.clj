9900648e973e93e5a3b292c0e90db26d61080f22
dcorsi <dcorsi@users.noreply.github.com>
2024-10-30 23:37:32 -0300

    Update core.clj

--- interbloqueo/src/interbloqueo/core.clj
+++ interbloqueo/src/interbloqueo/core.clj
@@ -3,28 +3,22 @@
 (def recurso-l (Object.))
 (def recurso-m (Object.))
 
-(defn hilo-a []
-  (Thread.
-    (fn []
-      (locking recurso-l
-        (print "Hilo A: Adquirió recurso L\n")(flush)
-        (Thread/sleep 2000)
-        (print "Hilo A: Esperando por recurso M\n")(flush)
-        (locking recurso-m
-          (print "Hilo A: Adquirió recurso M\n")(flush))))))
-
-(defn hilo-b []
-  (Thread.
-    (fn []
-      (locking recurso-m
-        (print "Hilo B: Adquirió recurso M\n")(flush)
-        (Thread/sleep 2000)
-        (print "Hilo B: Esperando por recurso L\n")(flush)
-        (locking recurso-l
-          (print "Hilo B: Adquirió recurso L\n")(flush))))))
-
 (defn -main []
-  (let [thread-a (hilo-a)
-        thread-b (hilo-b)]
+  (let [thread-a (Thread.
+                   (fn []
+                     (locking recurso-l
+                       (print "Hilo A: Adquirió recurso L\n")(flush)
+                       (Thread/sleep 2000)
+                       (print "Hilo A: Esperando por recurso M\n")(flush)
+                       (locking recurso-m
+                         (print "Hilo A: Adquirió recurso M\n")(flush)))))
+        thread-b (Thread.
+                   (fn []
+                     (locking recurso-m
+                       (print "Hilo B: Adquirió recurso M\n")(flush)
+                       (Thread/sleep 2000)
+                       (print "Hilo B: Esperando por recurso L\n")(flush)
+                       (locking recurso-l
+                         (print "Hilo B: Adquirió recurso L\n")(flush)))))]
     (.start thread-a)
     (.start thread-b)))
